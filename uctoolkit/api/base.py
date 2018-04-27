fad0866f615ee393ca04f499ed87b101b8a41460
jonathan.els <jonathanelscpt@gmail.com>
2018-04-27 13:26:34 +0200

    fixed bug in sql queries for no returned tuples

--- uctoolkit/api/base.py
+++ uctoolkit/api/base.py
@@ -233,7 +233,8 @@ class SimpleAXLAPI(BaseAXLAPI):
                                           searchCriteria=searchCriteria,
                                           returnedTags=returnedTags,
                                           skip=skip,
-                                          first=first)
+                                          first=first
+                                          )
         try:
             axl_list = serialize_object(axl_resp)["return"][self._return_name]
             return [self.object_factory(self.__class__.__name__, item) for item in axl_list]
@@ -315,11 +316,14 @@ class ThinAXLAPI(BaseAXLAPI):
                 serialized_resp = element_list_to_ordered_dict(
                     serialize_object(axl_resp)["return"]["rows"]
                 )
-            # AXL supplies different keyword for single row return
             except KeyError:
+                # single tuple response
                 serialized_resp = element_list_to_ordered_dict(
                     serialize_object(axl_resp)["return"]["row"]
                 )
+            except TypeError:
+                # no SQL tuples
+                serialized_resp = serialize_object(axl_resp)["return"]
             return self.object_factory(self.__class__.__name__, serialized_resp)
         except Fault as fault:
             raise IllegalSQLStatement(message=fault.message)
