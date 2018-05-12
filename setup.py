3809dfb6e34d9c0c17a554cf38b7f56476070747
jonathan.els <jonathanelscpt@gmail.com>
2018-05-12 19:31:52 +0200

    setup.py standardization on best practice

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
 # -*- coding: utf-8 -*-
 """setuptools based setup module."""
 
-import os
-from codecs import open
+import io
+from os.path import splitext
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from glob import glob
 from setuptools import setup, find_packages
 
 
-here = os.path.abspath(os.path.dirname(__file__))
+def read(*names, **kwargs):
+    return io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
 
-# packages = ['uctoolkit']
 
-requires = [
+install_requires = [
     'requests>=2.18.4',
     'urllib3>=1.22',
     'zeep>=2.5.0',
     'lxml>=4.2.1'
 ]
-# test_requirements = [
+# tests_require = [
 #     'pytest-httpbin==0.0.7',
 #     'pytest-cov',
 #     'pytest-mock',
@@ -25,34 +32,25 @@ requires = [
 #     'pytest>=2.8.0'
 # ]
 
-about = {}
-with open(os.path.join(here, 'uctoolkit', '__version__.py'), 'r', 'utf-8') as f:
-    exec(f.read(), about)
-
-with open('README.rst', 'r', 'utf-8') as f:
-    readme = f.read()
-
 
 setup(
-    name=about['__title__'],
-    version=about['__version__'],
-    description=about['__description__'],
-    long_description=readme,
-    author=about['__author__'],
-    author_email=about['__author_email__'],
-    url=about['__url__'],
+    name="UCToolkit",
+    version="0.0.1",
+    license="MIT",
+    description="Python Toolkit for Cisco UC APIs",
+    long_description=read('README.md'),
+    author="Jonathan Els",
+    author_email="jonathanelscpt@gmail.com",
+    url="https://github.com/jonathanelscpt/UCToolkit",
     python_requires=">=3.6.*",
-    packages=find_packages(include=['uctoolkit', 'uctoolkit.*']),
-    package_dir={'uctoolkit': 'uctoolkit'},
-    package_data={
-        '': ['LICENSE', 'README.md'],
-        'examples': ['*.py']
+    packages=find_packages('src'),
+    package_dir={
+        '': 'src'
     },
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
-    install_requires=requires,
-    # tests_require=test_requirements,
-    license=about['__license__'],
-    classifiers=(
+    zip_safe=False,
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
@@ -63,6 +61,13 @@ setup(
         'Topic :: Communications :: Telephony',
         'Topic :: Communications :: Conferencing',
         'Topic :: System :: Systems Administration'
-    ),
-    keywords='cisco cucm uc collaboration callmanager axl',
+    ],
+    keywords=[
+        "cisco", "cucm", "uc", "collaboration", "callmanager", "axl",
+    ],
+    install_requires=install_requires,
+    # tests_require=tests_require,
+    extras_require={},
+    entry_points={},
 )
+
