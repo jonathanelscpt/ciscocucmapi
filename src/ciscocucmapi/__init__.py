2579b161c9bb4b7cca8761706e78ec5b87ded109
jonathan.els <jonathanelscpt@gmail.com>
2018-05-12 20:39:40 +0200

    renamed pkg dir to ciscocucmapi. updated setup.py and readme

--- src/ciscocucmapi/__init__.py
+++ src/ciscocucmapi/__init__.py
@@ -14,7 +14,7 @@ from .__version__ import __title__, __description__, __url__, __version__
 from .__version__ import __build__, __author__, __author_email__, __license__
 from .__version__ import __copyright__
 
-from uctoolkit.connectors import (
+from ciscocucmapi.connectors import (
     UCMAXLConnector
 )
 
