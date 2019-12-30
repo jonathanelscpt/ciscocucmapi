622067f06ec65c7b2170b24a83d5972a9a28f54e
jonathanelscpt <jonathanelscpt@gmail.com>
2019-12-30 21:27:14 +0100

    setup flakes

--- setup.py
+++ setup.py
@@ -71,8 +71,8 @@ setup(
         'zeep>=2.5.0'
     ],
     extras_require={
-          'rst': ['docutils>=0.11'],
-        #   ':python_version=="2.6"': ['argparse'],
+        'rst': ['docutils>=0.11'],
+        # ':python_version=="2.6"': ['argparse'],
     },
     entry_points={
         'console_scripts': [
